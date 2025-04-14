2f15840a0d6a2f1572eac47b40e427c866146e30
Ritshidze Nedzimauli <u24717739@up.ac.za>
2025-04-14 20:00:35 +0200

    Add project files.

---
+++ S1P04/Controllers/PeopleController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u24717739_Practical_5.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public static List<Models.PersonModel> people = new List<Models.PersonModel>();
+        public ActionResult ListPeople()
+        {
+
+
+            people.Add(new Models.PersonModel
+            {
+                StuNumber = "u24717739",
+                FirstName = "Ritshidze",
+                LastName = "Nedzimauli",
+                Email = "u24717739@tuks.co.za",
+              
+            });
+            people.Add(new Models.PersonModel
+            {
+                StuNumber = "u24784606",
+                FirstName = "Msizi",
+                LastName = "Gumede",
+               Email = "u24784606@tuks.co.za",
+
+            });
+            people.Add(new Models.PersonModel
+            {
+                StuNumber = "u24634400",
+                FirstName = "Boitshoko",
+                LastName = "Mphahlele",
+                Email = "u24634400@tuks.co.za",             
+            });
+            people.Add(new Models.PersonModel
+            {
+               StuNumber = "u24724918",
+               FirstName = "Gundo",
+                LastName = "Ndou",
+                Email = "u24724918@tuks.co.za",       
+            });
+            people.Add(new Models.PersonModel
+            {
+                StuNumber = "u24924142",
+               FirstName = "Walter",
+               LastName = "Marimi",
+              Email = "u24924142@tuks.co.za",
+            });
+
+            return View(people);
+        }
+
+        
+        [HttpGet]
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Models.PersonModel pm)
+        {
+            people.Add(new Models.PersonModel { FirstName = pm.FirstName, LastName = pm.LastName, Email = pm.Email,  StuNumber = pm.StuNumber });
+            return RedirectToAction("ListPeople");
+
+        }
+    }
+}
---
+++ S1P04/Models/PersonModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace u24717739_Practical_5.Models
+{
+    public class PersonModel
+    {
+        [Display(Name = "Student number")] 
+        public string StuNumber { get; set; }
+
+        [Display(Name = "First Name")] 
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")] 
+        public string LastName { get; set; }
+
+        [Display(Name = "Email address")] 
+        public string Email { get; set; }
+
+       
+
+        public PersonModel()
+        {
+
+        }
+    }
+}
